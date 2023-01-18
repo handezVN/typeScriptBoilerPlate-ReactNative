1466d6fbcf5762646a8dc59648f22f03a6a276a7
handezVn <handez1008@gmail.com>
2023-01-18 10:19:14 +0700

    Login With FB Android

--- src/screens/UserDetails/LoginScreen.tsx
+++ src/screens/UserDetails/LoginScreen.tsx
@@ -1,15 +1,16 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {
   GoogleSignin,
   statusCodes,
   GoogleSigninButton,
 } from '@react-native-google-signin/google-signin';
-
+import {LoginButton, AccessToken, Settings} from 'react-native-fbsdk-next';
 GoogleSignin.configure({
   webClientId:
     '1023361931327-i9c0luuc6d1omiu2i62fifuc3r3jd541.apps.googleusercontent.com',
 });
+Settings.setAppID('APP ID');
 const LoginScreen = () => {
   const signIn = async () => {
     try {
@@ -30,11 +31,26 @@ const LoginScreen = () => {
     }
   };
   return (
-    <View>
+    <SafeAreaView>
       <Text>LoginScreen</Text>
       <Button title="Hello" onPress={() => signIn()} />
       <GoogleSigninButton />
-    </View>
+      <LoginButton
+        onLoginFinished={(error, result: any) => {
+          if (error) {
+            console.log('login has error: ' + result.error);
+          } else if (result.isCancelled) {
+            console.log('login is cancelled.');
+          } else {
+            console.log(result);
+            AccessToken.getCurrentAccessToken().then((data: any) => {
+              console.log(data.accessToken.toString());
+            });
+          }
+        }}
+        onLogoutFinished={() => console.log('logout.')}
+      />
+    </SafeAreaView>
   );
 };
 
