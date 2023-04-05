8eeaf0587065cb952f463f02da7cd0bb0210359e
Hoang Tuan Anh <74038883+handezVN@users.noreply.github.com>
2023-04-05 09:21:05 +0700

    Update asyncStorage.ts

--- src/utils/asyncStorage.ts
+++ src/utils/asyncStorage.ts
@@ -1,33 +1,35 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const setValueInAsyncStorage = async (key:string, value:any) => {
-    try {
-        if (key && value) {
-            let valueToStore = value;
-            if (typeof value === 'object') {
-                valueToStore = JSON.stringify(value);
-            }
-            await AsyncStorage.setItem(key, valueToStore);
-        }
-    } catch (error) {
-        console.log('error in asyncStorage -> storeData: ', error);
+export const setValueInAsyncStorage = async (key: string, value: any) => {
+  try {
+    if (key && value) {
+      let valueToStore = value;
+      if (typeof value !== 'string') {
+        valueToStore = JSON.stringify(value);
+      }
+      await AsyncStorage.setItem(key, valueToStore);
     }
+  } catch (error) {
+    console.log('error in asyncStorage -> storeData: ', error);
+  }
 };
 
-export const getValueFromAsyncStorage = async (key:string) => {
-    try {
-        if (key) {
-            const value:any = await AsyncStorage.getItem(key);
-            if (typeof value === 'object') {
-                return JSON.parse(value);
-            } else {
-                return value;
-            }
-        } else {
-            return null;
-        }
-    } catch (error) {
-        console.log('error in asyncStorage -> getStoredValue: ', error);
-        return null;
+export const getValueFromAsyncStorage = async (key: string) => {
+  try {
+    if (key) {
+      const value: any = await AsyncStorage.getItem(key);
+      console.log('value: ', value);
+      let parse = JSON.parse(value);
+      if (typeof parse !== 'string') {
+        return parse;
+      } else {
+        return value;
+      }
+    } else {
+      return null;
     }
+  } catch (error) {
+    console.log('error in asyncStorage -> getStoredValue: ', error);
+    return null;
+  }
 };
